69c73837387fd41120d636754825eb5efc2fd41e
Emmette Taylor <emmette.taylor@hotmail.com>
2023-02-20 15:08:53 +0800

    password generator version 1.0

--- password_gen/main.py
+++ password_gen/main.py
@@ -1,16 +1,17 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import random
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def character():
+    random_character = chr(random.randint(ord('a'), ord('z')))
+    return random_character
 
 
-# Press the green button in the gutter to run the script.
+def generate_password():
+    password = ""
+    for i in range(10):
+        password += character()
+    return password
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print(generate_password())
