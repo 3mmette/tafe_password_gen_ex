8eb7197f710a6e01d05fd325e1f074d1543b8ad7
Emmette Taylor <emmette.taylor@hotmail.com>
2023-02-20 15:57:49 +0800

    added uppercase, numbers and special characters

--- password_gen/main.py
+++ password_gen/main.py
@@ -1,15 +1,41 @@
 import random
 
 
-def character():
-    random_character = chr(random.randint(ord('a'), ord('z')))
-    return random_character
+def lower_character():
+    random_lower_character = chr(random.randint(ord('a'), ord('z')))
+    return random_lower_character
+
+
+def upper_character():
+    random_upper_character = chr(random.randint(ord('A'), ord('Z')))
+    return random_upper_character
+
+
+def number():
+    random_number = random.randint(0, 9)
+    return random_number
+
+
+def special_character():
+    special_characters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"]
+    character = random.choice(special_characters)
+    return character
 
 
 def generate_password(required_length):
     password = ""
     for i in range(required_length):
-        password += character()
+        character = int(random.randint(1, 4))
+        if character == 1:
+            password += lower_character()
+        elif character == 2:
+            password += upper_character()
+        elif character == 3:
+            password += str(number())
+        elif character == 4:
+            password += special_character()
+        else:
+            print("Error")
     return password
 
 
