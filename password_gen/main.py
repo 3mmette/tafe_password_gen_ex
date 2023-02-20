b8b9a5fc30dc7dd81762bb99c7f231706735967a
Emmette Taylor <emmette.taylor@hotmail.com>
2023-02-20 15:28:40 +0800

    custom password length

--- password_gen/main.py
+++ password_gen/main.py
@@ -6,12 +6,16 @@ def character():
     return random_character
 
 
-def generate_password():
+def generate_password(required_length):
     password = ""
-    for i in range(10):
+    for i in range(required_length):
         password += character()
     return password
 
 
 if __name__ == '__main__':
-    print(generate_password())
+    length = int(input("Password Length: "))
+    while not 0 < length < 50:
+        print("Invalid length. Must be between 0 and 50")
+        length = int(input("Password Length: "))
+    print(generate_password(length))
